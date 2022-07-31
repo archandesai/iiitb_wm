c2d2c298b3cd674242ffef16b57315a69a722fb6
archandesai <110079753+archandesai@users.noreply.github.com>
2022-07-31 22:48:34 +0530

    Update iiitb_wm_tb.v

--- iiitb_wm_tb.v
+++ iiitb_wm_tb.v
@@ -3,7 +3,7 @@ module iiitb_wm_tb();
 	wire door_lock, motor_on, fill_value_on, drain_value_on, done, soap_wash, water_wash; 
 	
 	
-automatic_washing_machine machine1(clk, reset, door_close, start, filled, detergent_added, cycle_timeout, drained, spin_timeout, door_lock, motor_on, fill_value_on, drain_value_on, done, soap_wash, water_wash);
+iiitb_wm machine1(clk, reset, door_close, start, filled, detergent_added, cycle_timeout, drained, spin_timeout, door_lock, motor_on, fill_value_on, drain_value_on, done, soap_wash, water_wash);
 
 
 	
