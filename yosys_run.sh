85fdec0a3b76aad656f3181d574301ee4ff0d5a9
Archan Desai <110079753+archandesai@users.noreply.github.com>
2022-08-02 18:19:20 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -7,7 +7,7 @@ synth -top iiitb_wm
 
 # mapping to mycells.lib
 dfflibmap -liberty /home/archan/iiitb_wm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/archan/iiitb_wm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/archan/iiitb_wm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 clean
 flatten
 # write synthesized design
